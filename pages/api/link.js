1654f5d6160bbcd4dec310a80c2ef2275cbd846c
ChristianLudovic <talchrist10@gmail.com>
2024-04-16 05:32:53 +0300

    please marche

--- pages/api/link.js
+++ pages/api/link.js
@@ -2,8 +2,10 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-export default async (req, res) => {
+const handle = async (req, res) => {
     const list = await prisma.link.findMany()
     res.json(list)
     
 }
+
+export default handle
